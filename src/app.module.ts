4c4b82da152a8e7b8d715d0cf56107f1e2155e4d
joaquimfirmo <joaquimnt18@gmail.com>
2024-10-13 20:51:26 -0300

    feat: cria common e database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule, CompanyModule],
   controllers: [AppController],
   providers: [AppService],
 })
