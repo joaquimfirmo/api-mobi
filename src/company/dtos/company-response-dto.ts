859db1b63a1443bdc0f1ff9e0c09d4607ed5e786
joaquimfirmo <joaquimnt18@gmail.com>
2024-12-28 16:44:06 -0300

    feat: deleta dtos nao utilizados.

--- src/company/dtos/company-response-dto.ts
+++
@@ -1,9 +0,0 @@
-export default class CompanyResponseDTO {
-  id: string;
-  razao_social: string;
-  nome_fantasia: string;
-  cnpj: string;
-  id_cidade: string;
-  created_at: Date | string;
-  updated_at: Date | string | null;
-}
--- src/company/dtos/create-company-response-dto.ts
+++
@@ -1,8 +0,0 @@
-export default class CreateCompanyResponseDTO {
-  id: string;
-  razao_social: string;
-  nome_fantasia: string;
-  cnpj: string;
-  id_cidade: string;
-  created_at: Date | string;
-}
