0766f55acf34a8c1c4074da9d62867d0ebe577d3
joaquimfirmo <joaquimnt18@gmail.com>
2025-03-01 16:13:16 -0300

    feat: add roles guards ao controller de company

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -7,16 +7,21 @@ import {
   Put,
   Delete,
   ParseUUIDPipe,
+  UseGuards,
 } from '@nestjs/common';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { Roles } from '../common/decorators/roles.decorator';
 import { CompanyService } from './company.service';
 import Company from './entities/company.entity';
 import { CreateCompanyRequestDTO, UpdateCompanyRequestDTO } from './dtos';
 
 @Controller('empresas')
+@UseGuards(RolesGuard)
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
   @Get()
+  @Roles(['admin'])
   async findAll(): Promise<Company[]> {
     return this.companyService.findAll();
   }
