41ad41978b5c1ef7df0e8f4a715566823517d749
joaquimfirmo <joaquimnt18@gmail.com>
2025-03-01 16:13:50 -0300

    refactor: refatora controller de usuarios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,31 +11,31 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
+@Controller('usuarios')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post('user')
-  create(@Body() createUserDto: CreateUserDto): Promise<any> {
-    return this.usersService.create(createUserDto);
-  }
 
   @Get()
   findAll(): Promise<any> {
     return this.usersService.findAll();
   }
 
-  @Get('user/:id')
+  @Get('usuario/:id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
-  @Patch('user/:id')
+  @Post('usuario')
+  create(@Body() createUserDto: CreateUserDto): Promise<any> {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Patch('usuario/:id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
-  @Delete('user/:id')
+  @Delete('usuario/:id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
