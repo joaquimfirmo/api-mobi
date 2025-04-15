8ad41ef02040c401b3fe778296213c00296c6192
Joaquim Firmo <joaquimnt18@gmail.com>
2025-04-15 16:03:29 -0300

    test: cria testes unitarios para o transports.controller e para o transports.service.

--- src/transports/transports.controller.spec.ts
+++ src/transports/transports.controller.spec.ts
@@ -4,6 +4,21 @@ import { TransportsService } from './transports.service';
 
 describe('TransportsController', () => {
   let controller: TransportsController;
+  let service: TransportsService;
+
+  const mockTransports = [
+    {
+      rota: 'Cidade A - Cidade B',
+      empresa: 'Empresa A',
+      distancia_km: '60',
+      duracao: {
+        hours: 1,
+      },
+      veiculo: 'Van',
+      horario_partida: '07:00:00',
+      horario_chegada: '08:00:00',
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,9 +34,27 @@ describe('TransportsController', () => {
     }).compile();
 
     controller = module.get<TransportsController>(TransportsController);
+    service = module.get<TransportsService>(TransportsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call findAll method from service', async () => {
+    const filters = {
+      page: 1,
+      limit: 10,
+      diaSemana: null,
+      horaPartida: null,
+      idCidadeOrigem: null,
+      idCidadeDestino: null,
+    };
+    jest.spyOn(service, 'findAll').mockResolvedValueOnce(mockTransports);
+    expect(await controller.findAll(filters)).toBe(mockTransports);
+    expect(service.findAll).toHaveBeenCalledTimes(1);
+
+    const { page, limit, ...rest } = filters;
+    expect(service.findAll).toHaveBeenCalledWith(rest, page, limit);
   });
 });
--- src/transports/transports.service.spec.ts
+++ src/transports/transports.service.spec.ts
@@ -2,9 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Logger, Scope } from '@nestjs/common';
 import { TransportsService } from './transports.service';
 import { TransportsRepository } from './transports.repository';
+import { DiaSemana } from './dto/filters-transport.dto';
 
 describe('TransportsService', () => {
   let service: TransportsService;
+  let repository: TransportsRepository;
+  let logger: Logger;
+
+  const mockTransports = [
+    {
+      rota: 'Cidade A - Cidade B',
+      empresa: 'Empresa A',
+      distancia_km: '60',
+      duracao: {
+        hours: 1,
+      },
+      veiculo: 'Van',
+      horario_partida: '07:00:00',
+      horario_chegada: '08:00:00',
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,9 +45,42 @@ describe('TransportsService', () => {
     }).compile();
 
     service = module.get<TransportsService>(TransportsService);
+    repository = module.get<TransportsRepository>(TransportsRepository);
+    logger = module.get<Logger>(Logger);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call findAll method from repository', async () => {
+    jest.spyOn(repository, 'findAll').mockResolvedValueOnce(mockTransports);
+    const filters = {
+      DiaSemana: DiaSemana.SegundaFeira,
+    };
+
+    const result = await service.findAll(filters, 1, 10);
+    expect(repository.findAll).toHaveBeenCalledWith(filters, 1, 10);
+    expect(repository.findAll).toHaveBeenCalledTimes(1);
+    expect(logger.log).toHaveBeenCalledWith(
+      `Fetching transports with filters: ${JSON.stringify(filters)}`,
+    );
+    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(mockTransports);
+  });
+
+  it('should log error when findAll fails', async () => {
+    const errorMessage = 'Error fetching transports';
+    jest
+      .spyOn(repository, 'findAll')
+      .mockRejectedValueOnce(new Error(errorMessage));
+    const filters = {
+      DiaSemana: DiaSemana.SegundaFeira,
+    };
+
+    await service.findAll(filters, 1, 10);
+    expect(logger.error).toHaveBeenCalledWith(
+      `Error fetching transports: ${errorMessage}`,
+    );
   });
 });
