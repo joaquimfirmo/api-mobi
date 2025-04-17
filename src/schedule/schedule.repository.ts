2a236e7d12ca38a93929b4fa308f23e2e9787cdd
Joaquim Firmo <joaquimnt18@gmail.com>
2025-04-17 19:29:14 -0300

    refactor: refatora shedule.repository.

--- src/schedule/schedule.repository.ts
+++ src/schedule/schedule.repository.ts
@@ -1,51 +1,64 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Kysely, sql } from 'kysely';
-import { Database } from 'src/common/database/types';
-// import { CreateScheduleDto } from './dto/create-schedule.dto';
-// import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import { Database, Schedules } from 'src/common/database/types';
 import { Schedule } from './entities/schedule.entity';
+import { ScheduleMapper } from './mapper/schedule.mapper';
 
 @Injectable()
 export class ScheduleRepository {
   constructor(@Inject('DATABASE_CONNECTION') private db: Kysely<Database>) {}
 
-  async findAll() {
-    return await this.db.selectFrom('horarios').selectAll().execute();
+  async findAll(
+    criteria: Partial<Schedules>,
+    page: number,
+    limit: number,
+  ): Promise<Schedule[]> {
+    const query = this.buildQuery(criteria);
+    const offset = page * limit;
+    const schedules = await query
+      .selectAll()
+      .limit(limit)
+      .offset(offset)
+      .execute();
+
+    return schedules.map((schedule) => {
+      return ScheduleMapper.toDomain(schedule);
+    });
   }
 
   async findById(id: string) {
-    return await this.db
+    const [result] = await this.db
       .selectFrom('horarios')
       .selectAll()
       .where('id', '=', id)
       .limit(1)
       .execute();
+
+    if (result?.created_at) {
+      return ScheduleMapper.toDomain(result);
+    }
+
+    return null;
   }
-  async findScheduleByHoursAndRoute(
-    horaPartida: string,
-    horaChegada: string,
-    idRota: string,
-  ) {
-    return await this.db
-      .selectFrom('horarios')
-      .where('hora_partida', '=', horaPartida)
-      .where('hora_chegada', '=', horaChegada)
-      .where('id_rota', '=', idRota)
-      .execute();
+  async findScheduleByHoursAndRoute(criteria: Partial<Schedules>) {
+    const query = this.buildQuery(criteria);
+    return await query.selectAll().execute();
   }
 
-  async create(schedule: Schedule) {
-    return await this.db.transaction().execute(async (trx) => {
+  async create(schedule: Partial<Schedules>): Promise<Schedule> {
+    const record = await this.db.transaction().execute(async (trx) => {
       return await trx
         .insertInto('horarios')
         .values({
           id: schedule.id,
-          hora_partida: schedule.horaPartida,
-          hora_chegada: schedule.horaChegada,
-          id_rota: schedule.idRota,
+          dia_semana: schedule.dia_semana,
+          hora_partida: schedule.hora_partida,
+          hora_chegada: schedule.hora_chegada,
+          id_rota: schedule.id_rota,
         })
         .returning([
           'id',
+          'dia_semana',
           'hora_partida',
           'hora_chegada',
           'id_rota',
@@ -54,21 +67,25 @@ export class ScheduleRepository {
         ])
         .executeTakeFirstOrThrow();
     });
+    return ScheduleMapper.toDomain(record);
   }
 
-  async update(id: string, schedule: Schedule) {
+  async update(id: string, schedule: Partial<Schedules>) {
+    console.log('schedule', schedule);
     return await this.db.transaction().execute(async (trx) => {
       return await trx
         .updateTable('horarios')
         .set({
-          ...(schedule.horaPartida && { hora_partida: schedule.horaPartida }),
-          ...(schedule.horaChegada && { hora_chegada: schedule.horaChegada }),
-          ...(schedule.idRota && { id_rota: schedule.idRota }),
+          ...{ dia_semana: schedule.dia_semana },
+          ...{ hora_partida: schedule.hora_partida },
+          ...{ hora_chegada: schedule.hora_chegada },
+          ...{ id_rota: schedule.id_rota },
           updated_at: sql`now()`,
         })
         .where('id', '=', id)
         .returning([
           'id',
+          'dia_semana',
           'hora_partida',
           'hora_chegada',
           'id_rota',
@@ -87,5 +104,27 @@ export class ScheduleRepository {
         .returning(['id'])
         .executeTakeFirstOrThrow();
     });
+  }
+
+  private buildQuery(criteria: Partial<Schedules>) {
+    let query = this.db.selectFrom('horarios');
+
+    if (criteria.dia_semana) {
+      query = query.where('dia_semana', '=', criteria.dia_semana);
+    }
+
+    if (criteria.hora_partida) {
+      query = query.where('hora_partida', '=', criteria.hora_partida);
+    }
+
+    if (criteria.hora_chegada) {
+      query = query.where('hora_chegada', '=', criteria.hora_chegada);
+    }
+
+    if (criteria.id_rota) {
+      query = query.where('id_rota', '=', criteria.id_rota);
+    }
+
+    return query;
   }
 }
