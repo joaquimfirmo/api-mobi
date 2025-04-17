4b8e1afae042d9f3db070799a9e4db1440526f44
Joaquim Firmo <joaquimnt18@gmail.com>
2025-04-17 19:34:24 -0300

    feat: cria novo campo no schedule.create.dto.

--- src/schedule/dto/create-schedule.dto.ts
+++ src/schedule/dto/create-schedule.dto.ts
@@ -1,6 +1,12 @@
-import { IsString, Matches, IsUUID } from 'class-validator';
+import { IsString, Matches, IsUUID, IsEnum } from 'class-validator';
+import { DiasSemana } from '../../types/enums/dias-semana.enum';
 
 export class CreateScheduleDto {
+  @IsEnum(DiasSemana, {
+    message:
+      'O dia da semana deve ser um dos seguintes: Domingo, Segunda-feira, Terça-feira, Quarta-feira, Quinta-feira, Sexta-feira ou Sábado.',
+  })
+  public readonly diaSemana: DiasSemana;
   @IsString()
   @Matches(/^([0-9]{2})\:([0-9]{2})\:([0-9]{2})$/)
   public readonly horaPartida: string;
