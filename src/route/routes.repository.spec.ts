bdc66f3800df7c9408d554419948eef6ecc80451
Joaquim Firmo <joaquimnt18@gmail.com>
2025-04-22 14:29:32 -0300

    test: ajusta  testes unitarios para routes.repository.

--- src/route/routes.repository.spec.ts
+++ src/route/routes.repository.spec.ts
@@ -2,10 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RoutesRepository } from './routes.repository';
 import { Kysely } from 'kysely';
 import { Database } from 'src/common/database/types';
+import { RouteMapper } from './mapper/route.mapper';
 
 describe('RoutesRepository', () => {
   let repository: RoutesRepository;
   let db: Kysely<Database>;
+
+  const mockRoutes = [
+    {
+      id: '1',
+      nome: 'Rota 1',
+      id_cidade_origem: 'Cidade A',
+      id_cidade_destino: 'Cidade B',
+      distancia: 100,
+      tempo_estimado: '01:00',
+      local: 'Local A',
+      via_principal: 'Via Principal A',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -39,12 +55,28 @@ describe('RoutesRepository', () => {
   });
 
   it('should call findAll method', async () => {
-    await repository.findAll();
+    jest.spyOn(repository as any, 'buildQuery');
+
+    db.selectFrom('rotas').selectAll().execute = jest
+      .fn()
+      .mockResolvedValue(mockRoutes);
+
+    const result = await repository.findAll({}, 0, 10);
     expect(db.selectFrom('rotas').selectAll().execute).toHaveBeenCalled();
+    expect(db.selectFrom('rotas').selectAll().limit).toHaveBeenCalledWith(10);
+    expect(db.selectFrom('rotas').selectAll().offset).toHaveBeenCalledWith(0);
+    expect(repository['buildQuery']).toHaveBeenCalledWith({});
+    expect(result).toEqual(
+      mockRoutes.map((route) => RouteMapper.toDomain(route)),
+    );
   });
 
   it('should call findById method', async () => {
-    await repository.findById('1');
+    db.selectFrom('rotas').selectAll().where('id', '=', '1').execute = jest
+      .fn()
+      .mockResolvedValue([mockRoutes[0]]);
+
+    const result = await repository.findById('1');
     expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
       'id',
       '=',
@@ -52,27 +84,125 @@ describe('RoutesRepository', () => {
     );
     expect(db.selectFrom('rotas').selectAll().limit).toHaveBeenCalledWith(1);
     expect(db.selectFrom('rotas').selectAll().execute).toHaveBeenCalled();
+    expect(result).toEqual(RouteMapper.toDomain(mockRoutes[0]));
+  });
+
+  it('should call findRouteByCities method', async () => {
+    const cityId1 = 'city-id-1';
+    const cityId2 = 'city-id-2';
+
+    db
+      .selectFrom('rotas')
+      .selectAll()
+      .where('id_cidade_origem', '=', cityId1)
+      .where('id_cidade_destino', '=', cityId2).execute = jest
+      .fn()
+      .mockResolvedValue([mockRoutes[0]]);
+
+    const result = await repository.findRouteByCities(cityId1, cityId2);
+
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'id_cidade_origem',
+      '=',
+      cityId1,
+    );
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'id_cidade_destino',
+      '=',
+      cityId2,
+    );
+    expect(db.selectFrom('rotas').selectAll().limit).toHaveBeenCalledWith(1);
+    expect(db.selectFrom('rotas').selectAll().execute).toHaveBeenCalled();
+    expect(result).toEqual(RouteMapper.toDomain(mockRoutes[0]));
+  });
+
+  it('shoud call findRouteByName method', async () => {
+    const routeName = 'City A to City B';
+
+    db.selectFrom('rotas').selectAll().where('nome', '=', routeName).execute =
+      jest.fn().mockResolvedValue([mockRoutes[0]]);
+
+    const result = await repository.findRouteByName(routeName);
+
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'nome',
+      '=',
+      routeName,
+    );
+    expect(db.selectFrom('rotas').selectAll().limit).toHaveBeenCalledWith(1);
+    expect(db.selectFrom('rotas').selectAll().execute).toHaveBeenCalled();
+    expect(result).toEqual(RouteMapper.toDomain(mockRoutes[0]));
   });
 
   it('should call create method', async () => {
-    await repository.create({
-      name: 'Nome',
-      idOriginCity: 'idCidadeOrigem',
-      idDestinationCity: 'idCidadeDestino',
-      distance: 100,
-      estimatedTime: '01:00',
-      originLocation: 'localOrigem',
-      id: '1',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
+    const route = {
+      nome: 'City 1 to City 2',
+      idCidadeOrigem: 'fake-id-city-1',
+      idCidadeDestino: 'fake-id-city-2',
+      distancia: 60,
+      tempoEstimado: '1 hours',
+      localOrigem: 'City 1',
+      viaPrincipal: 'Main Road',
+    };
+
+    db.transaction().execute = jest.fn().mockResolvedValue([
+      {
+        nome: route.nome,
+        id_cidade_origem: route.idCidadeOrigem,
+        id_cidade_destino: route.idCidadeDestino,
+        distancia: route.distancia,
+        tempo_estimado: route.tempoEstimado,
+        local: route.localOrigem,
+        via_principal: route.viaPrincipal,
+        id: '1',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+
+    await repository.create(RouteMapper.toPersistence(route));
+
     expect(db.transaction().execute).toHaveBeenCalledWith(expect.any(Function));
+    expect(db.transaction().execute).toHaveBeenCalledTimes(1);
   });
 
   it('should call update method', async () => {
-    await repository.update('1', {
-      idCidadeOrigem: 'Cidade Origem',
+    const routeForUpdate = {
+      nome: 'City 1 to City 2',
+      idCidadeOrigem: 'fake-id-city-1',
+      idCidadeDestino: 'fake-id-city-2',
+      distancia: 60,
+      tempoEstimado: '1 hours',
+      localOrigem: 'City 1',
+      viaPrincipal: 'Main Road',
+      id: '1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const record = {
+      id: routeForUpdate.id,
+      nome: routeForUpdate.nome,
+      id_cidade_origem: routeForUpdate.idCidadeOrigem,
+      id_cidade_destino: routeForUpdate.idCidadeDestino,
+      distancia: routeForUpdate.distancia,
+      tempo_estimado: routeForUpdate.tempoEstimado,
+      local: routeForUpdate.localOrigem,
+      via_principal: routeForUpdate.viaPrincipal,
+      created_at: routeForUpdate.createdAt,
+      updated_at: routeForUpdate.updatedAt,
+    };
+
+    db.transaction = jest.fn().mockReturnValue({
+      execute: jest.fn().mockResolvedValue(record),
     });
+
+    const result = await repository.update(
+      '1',
+      RouteMapper.toPersistence(routeForUpdate),
+    );
+
+    expect(result).toEqual(RouteMapper.toDomain(record));
     expect(db.transaction().execute).toHaveBeenCalledWith(expect.any(Function));
   });
 
@@ -81,70 +211,58 @@ describe('RoutesRepository', () => {
     expect(db.transaction().execute).toHaveBeenCalledWith(expect.any(Function));
   });
 
-  // it.skip('should call findAllTransportsByCityId method', async () => {
-  //   await repository.findAllTransportsByCityId('1');
-  //   expect(db.selectFrom('transportes').selectAll().where).toHaveBeenCalledWith(
-  //     'id_cidade',
-  //     '=',
-  //     '1',
-  //   );
+  it('buildQuery should be called with the correct parameters', () => {
+    const criteria = {
+      nome: 'Rota 1',
+      id_cidade_origem: 'Cidade A',
+      id_cidade_destino: 'Cidade B',
+      distancia: 100,
+      tempo_estimado: '01:00',
+      local: 'Local A',
+      via_principal: 'Via Principal A',
+    };
 
-  //   expect(db.selectFrom('transportes').selectAll().execute).toHaveBeenCalled();
-  // });
+    repository['buildQuery'](criteria);
 
-  // it.skip('should call findByCityId method with the correct filters ', async () => {
-  //   const spy = jest.spyOn(repository, 'generateWhereClause');
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'nome',
+      '=',
+      criteria.nome,
+    );
 
-  //   await repository.findByCityId('1', 0, 20, {
-  //     day: '1',
-  //     hour: '12:00',
-  //     city_destination: '1',
-  //   });
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'id_cidade_origem',
+      '=',
+      criteria.id_cidade_origem,
+    );
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'id_cidade_destino',
+      '=',
+      criteria.id_cidade_destino,
+    );
 
-  //   expect(spy).toHaveBeenCalledWith('1', '12:00', '1');
-  //   expect(
-  //     db.selectFrom('transportes').selectAll().innerJoin,
-  //   ).toHaveBeenCalledWith('veiculos', 'veiculos.id', 'transportes.id_veiculo');
-  //   expect(
-  //     db.selectFrom('transportes').selectAll().innerJoin,
-  //   ).toHaveBeenCalledWith('empresas', 'empresas.id', 'transportes.id_empresa');
-  //   expect(db.selectFrom('transportes').selectAll().where).toHaveBeenCalledWith(
-  //     'transportes.id_cidade',
-  //     '=',
-  //     '1',
-  //   );
-  //   expect(db.selectFrom('transportes').selectAll().limit).toHaveBeenCalledWith(
-  //     20,
-  //   );
-  //   expect(
-  //     db.selectFrom('transportes').selectAll().offset,
-  //   ).toHaveBeenCalledWith(0);
-  //   expect(db.selectFrom('transportes').compile).toHaveBeenCalled();
-  // });
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'distancia',
+      '=',
+      criteria.distancia,
+    );
 
-  // it.skip('should call findByCityId method without filters', async () => {
-  //   const spy = jest.spyOn(repository, 'generateWhereClause');
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'tempo_estimado',
+      '=',
+      criteria.tempo_estimado,
+    );
 
-  //   await repository.findByCityId('1', 0, 20, {});
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'local',
+      '=',
+      criteria.local,
+    );
 
-  //   expect(spy).toHaveBeenCalledWith(undefined, undefined, undefined);
-  //   expect(
-  //     db.selectFrom('transportes').selectAll().innerJoin,
-  //   ).toHaveBeenCalledWith('veiculos', 'veiculos.id', 'transportes.id_veiculo');
-  //   expect(
-  //     db.selectFrom('transportes').selectAll().innerJoin,
-  //   ).toHaveBeenCalledWith('empresas', 'empresas.id', 'transportes.id_empresa');
-  //   expect(db.selectFrom('transportes').selectAll().where).toHaveBeenCalledWith(
-  //     'transportes.id_cidade',
-  //     '=',
-  //     '1',
-  //   );
-  //   expect(db.selectFrom('transportes').selectAll().limit).toHaveBeenCalledWith(
-  //     20,
-  //   );
-  //   expect(
-  //     db.selectFrom('transportes').selectAll().offset,
-  //   ).toHaveBeenCalledWith(0);
-  //   expect(db.selectFrom('transportes').compile).toHaveBeenCalled();
-  // });
+    expect(db.selectFrom('rotas').selectAll().where).toHaveBeenCalledWith(
+      'via_principal',
+      '=',
+      criteria.via_principal,
+    );
+  });
 });
