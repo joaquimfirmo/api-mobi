2a2a3171ddc39947c52ff4d36ef794ce18a3dfe4
Joaquim Firmo <joaquimnt18@gmail.com>
2025-04-29 17:57:00 -0300

    test: refatora testes unitarios de vehicles repository e service.

--- src/vehicles/vehicles.repository.spec.ts
+++ src/vehicles/vehicles.repository.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VehiclesRepository } from './vehicles.repository';
 import { Kysely } from 'kysely';
 import { Database } from 'src/common/database/types';
+import { DatabaseException } from '../common/execptions/database.execption';
 
 describe('VehiclesRepository', () => {
   let repository: VehiclesRepository;
@@ -38,7 +39,22 @@ describe('VehiclesRepository', () => {
     expect(db.selectFrom('veiculos').selectAll().execute).toHaveBeenCalled();
   });
 
+  it('should throw DatabaseException if findAll fails', async () => {
+    db.selectFrom('veiculos').selectAll().execute = jest
+      .fn()
+      .mockRejectedValue(new Error('Database error'));
+    await expect(repository.findAll()).rejects.toThrow(DatabaseException);
+    await expect(repository.findAll()).rejects.toThrow(
+      'Não foi possível buscar veículos',
+    );
+  });
+
   it('should call findById method', async () => {
+    db.selectFrom('veiculos').selectAll().where('id', '=', '1').execute = jest
+      .fn()
+      .mockResolvedValue([
+        { id: '1', nome: 'Nome', created_at: new Date(), updated_at: '' },
+      ]);
     await repository.findById('1');
     expect(db.selectFrom('veiculos').selectAll().where).toHaveBeenCalledWith(
       'id',
@@ -49,7 +65,23 @@ describe('VehiclesRepository', () => {
     expect(db.selectFrom('veiculos').selectAll().execute).toHaveBeenCalled();
   });
 
+  it('should throw DatabaseException if findById fails', async () => {
+    db.selectFrom('veiculos').selectAll().where('id', '=', '1').execute = jest
+      .fn()
+      .mockRejectedValue(new Error('Database error'));
+    await expect(repository.findById('1')).rejects.toThrow(DatabaseException);
+    await expect(repository.findById('1')).rejects.toThrow(
+      'Não foi possível buscar veículo com o id 1',
+    );
+  });
+
   it('should call findByName method', async () => {
+    db.selectFrom('veiculos').selectAll().where('nome', '=', 'Nome').execute =
+      jest
+        .fn()
+        .mockResolvedValue([
+          { id: '1', nome: 'Nome', created_at: new Date(), updated_at: '' },
+        ]);
     await repository.findByName('Nome');
     expect(db.selectFrom('veiculos').selectAll().where).toHaveBeenCalledWith(
       'nome',
@@ -60,7 +92,24 @@ describe('VehiclesRepository', () => {
     expect(db.selectFrom('veiculos').selectAll().execute).toHaveBeenCalled();
   });
 
+  it('should throw DatabaseException if findByName fails', async () => {
+    db.selectFrom('veiculos').selectAll().where('nome', '=', 'Nome').execute =
+      jest.fn().mockRejectedValue(new Error('Database error'));
+    await expect(repository.findByName('Nome')).rejects.toThrow(
+      DatabaseException,
+    );
+    await expect(repository.findByName('Nome')).rejects.toThrow(
+      'Não foi possível buscar veículo com o nome Nome',
+    );
+  });
+
   it('should call create method', async () => {
+    db.transaction().execute = jest
+      .fn()
+      .mockResolvedValue([
+        { id: '1', nome: 'Nome', created_at: new Date(), updated_at: '' },
+      ]);
+
     await repository.create({
       id: '1',
       nome: 'Nome',
@@ -71,6 +120,28 @@ describe('VehiclesRepository', () => {
     expect(db.transaction().execute).toHaveBeenCalledWith(expect.any(Function));
   });
 
+  it('should throw DatabaseException if create fails', async () => {
+    db.transaction().execute = jest
+      .fn()
+      .mockRejectedValue(new Error('Database error'));
+    await expect(
+      repository.create({
+        id: '1',
+        nome: 'Nome',
+        created_at: new Date(),
+        updated_at: '',
+      }),
+    ).rejects.toThrow(DatabaseException);
+    await expect(
+      repository.create({
+        id: '1',
+        nome: 'Nome',
+        created_at: new Date(),
+        updated_at: '',
+      }),
+    ).rejects.toThrow('Não foi possível criar veículo com o nome Nome');
+  });
+
   it('should call update method', async () => {
     await repository.update('1', {
       nome: 'Nome',
@@ -79,9 +150,37 @@ describe('VehiclesRepository', () => {
     expect(db.transaction().execute).toHaveBeenCalledWith(expect.any(Function));
   });
 
+  it('should throw DatabaseException if update fails', async () => {
+    db.transaction().execute = jest
+      .fn()
+      .mockRejectedValue(new Error('Database error'));
+    await expect(
+      repository.update('1', {
+        nome: 'Nome',
+      }),
+    ).rejects.toThrow(DatabaseException);
+    await expect(
+      repository.update('1', {
+        nome: 'Nome',
+      }),
+    ).rejects.toThrow(
+      'Não foi possível atualizar veículo com o ID 1: Database error',
+    );
+  });
+
   it('should call delete method', async () => {
     await repository.delete('1');
     expect(db.transaction().execute).toHaveBeenCalledWith(expect.any(Function));
     expect(db.transaction().execute).toHaveBeenCalledWith(expect.any(Function));
+  });
+
+  it('should throw DatabaseException if delete fails', async () => {
+    db.transaction().execute = jest
+      .fn()
+      .mockRejectedValue(new Error('Database error'));
+    await expect(repository.delete('1')).rejects.toThrow(DatabaseException);
+    await expect(repository.delete('1')).rejects.toThrow(
+      'Não foi possível deletar veículo com o ID 1: Database error',
+    );
   });
 });
--- src/vehicles/vehicles.service.spec.ts
+++ src/vehicles/vehicles.service.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Logger, Scope } from '@nestjs/common';
+import {
+  Logger,
+  Scope,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { VehiclesRepository } from './vehicles.repository';
 
@@ -62,7 +67,7 @@ describe('VehiclesService', () => {
 
   it('should call findOne method from repository', async () => {
     const vehicleId = '1';
-    jest.spyOn(repository, 'findById').mockResolvedValueOnce([mockVehicles[0]]);
+    jest.spyOn(repository, 'findById').mockResolvedValueOnce(mockVehicles[0]);
     const vehicle = await service.findOne(vehicleId);
     expect(vehicle).toEqual(mockVehicles[0]);
     expect(repository.findById).toHaveBeenCalledWith(vehicleId);
@@ -72,8 +77,8 @@ describe('VehiclesService', () => {
     const createVehicleDto = {
       nome: 'Carro A',
     };
-    jest.spyOn(repository, 'findByName').mockResolvedValueOnce([]);
-    jest.spyOn(repository, 'create').mockResolvedValueOnce([mockVehicles[0]]);
+    jest.spyOn(repository, 'findByName').mockResolvedValueOnce(null);
+    jest.spyOn(repository, 'create').mockResolvedValueOnce(mockVehicles[0]);
 
     const vehicle = await service.create(createVehicleDto);
 
@@ -98,17 +103,15 @@ describe('VehiclesService', () => {
     const createVehicleDto = {
       nome: 'Carro A',
     };
-    jest
-      .spyOn(repository, 'findByName')
-      .mockResolvedValueOnce([mockVehicles[0]]);
+    jest.spyOn(repository, 'findByName').mockResolvedValueOnce(mockVehicles[0]);
 
     await expect(service.create(createVehicleDto)).rejects.toThrowError(
-      `Veículo com o nome: ${createVehicleDto.nome} já existe`,
+      `Veículo com o nome:${createVehicleDto.nome}, já existe`,
     );
     expect(repository.findByName).toHaveBeenCalledWith(createVehicleDto.nome);
     expect(repository.create).not.toHaveBeenCalled();
     expect(logger.error).toHaveBeenCalledWith(
-      `Veículo com o nome: ${createVehicleDto.nome} já existe`,
+      `Veículo com o nome:${createVehicleDto.nome}, já existe`,
     );
   });
 
@@ -117,7 +120,7 @@ describe('VehiclesService', () => {
     const updateVehicleDto = {
       nome: 'Carro B',
     };
-    jest.spyOn(repository, 'findById').mockResolvedValueOnce([mockVehicles[0]]);
+    jest.spyOn(repository, 'findById').mockResolvedValueOnce(mockVehicles[0]);
     jest.spyOn(repository, 'update').mockResolvedValueOnce(mockVehicles[0]);
     const vehicle = await service.update(vehicleId, updateVehicleDto);
     expect(vehicle).toEqual(mockVehicles[0]);
@@ -133,27 +136,25 @@ describe('VehiclesService', () => {
     const updateVehicleDto = {
       nome: 'Carro B',
     };
-    jest.spyOn(repository, 'findById').mockResolvedValueOnce([]);
+    jest.spyOn(repository, 'findById').mockResolvedValueOnce(null);
 
     await expect(
       service.update(vehicleId, updateVehicleDto),
     ).rejects.toThrowError(
-      `Veículo com o ID:${vehicleId} informado não foi encontrado`,
+      `Veículo com o ID:${vehicleId}, informado não foi encontrado`,
     );
     expect(repository.findById).toHaveBeenCalledWith(vehicleId);
     expect(repository.update).not.toHaveBeenCalled();
     expect(logger.error).toHaveBeenCalledWith(
-      `Veículo com o ID:${vehicleId} informado não foi encontrado`,
+      `Veículo com o ID:${vehicleId}, informado não foi encontrado`,
     );
   });
 
   it('should call delete method from repository', async () => {
     const vehicleId = '1';
-    jest.spyOn(repository, 'delete').mockResolvedValueOnce({
-      id: vehicleId,
-    });
+    jest.spyOn(repository, 'delete').mockResolvedValueOnce(undefined);
 
-    jest.spyOn(repository, 'findById').mockResolvedValueOnce([mockVehicles[0]]);
+    jest.spyOn(repository, 'findById').mockResolvedValueOnce(mockVehicles[0]);
     await service.remove(vehicleId);
     expect(repository.delete).toHaveBeenCalledWith(vehicleId);
     expect(logger.log).toHaveBeenCalledWith(
@@ -164,31 +165,65 @@ describe('VehiclesService', () => {
     );
   });
 
-  describe('verifyVehicle', () => {
-    it('should call checkVehicleById method', async () => {
+  describe('getVehicleById', () => {
+    it('should call findById method from repository', async () => {
       const vehicleId = '1';
-      jest
-        .spyOn(service as any, 'checkVehicleById')
-        .mockResolvedValueOnce(true);
-      const result = await (service as any).verifyVehicleExist({
-        id: vehicleId,
-      });
-      expect(result).toBe(true);
-      expect((service as any).checkVehicleById).toHaveBeenCalledWith(vehicleId);
+      jest.spyOn(repository, 'findById').mockResolvedValueOnce(mockVehicles[0]);
+      const vehicle = await (service as any).getVehicleById(vehicleId);
+      expect(vehicle).toEqual(mockVehicles[0]);
+      expect(repository.findById).toHaveBeenCalledWith(vehicleId);
     });
 
-    it('should call checkVehicleByName method', async () => {
+    it('should throw NotFoundException if vehicle does not exist', async () => {
+      const vehicleId = '1';
+      jest.spyOn(repository, 'findById').mockResolvedValueOnce(null);
+      await expect((service as any).getVehicleById(vehicleId)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('ensureVehicleDoesNotExist', () => {
+    it('should throw BadRequestException if vehicle already exists', async () => {
+      const createVehicleDto = {
+        nome: 'Carro A',
+      };
+      jest
+        .spyOn(repository, 'findByName')
+        .mockResolvedValueOnce(mockVehicles[0]);
+      await expect(
+        (service as any).ensureVehicleDoesNotExist(createVehicleDto),
+      ).rejects.toThrow(BadRequestException);
+    });
+
+    it('should not throw if vehicle does not exist', async () => {
+      const createVehicleDto = {
+        nome: 'Carro B',
+      };
+      jest.spyOn(repository, 'findByName').mockResolvedValueOnce(null);
+      await expect(
+        (service as any).ensureVehicleDoesNotExist(createVehicleDto),
+      ).resolves.not.toThrow();
+    });
+  });
+
+  describe('checkVehicleByName', () => {
+    it('should return true if vehicle exists', async () => {
       const vehicleName = 'Carro A';
       jest
-        .spyOn(service as any, 'checkVehicleByName')
-        .mockResolvedValueOnce(true);
-      const result = await (service as any).verifyVehicleExist({
-        nome: vehicleName,
-      });
+        .spyOn(repository, 'findByName')
+        .mockResolvedValueOnce(mockVehicles[0]);
+      const result = await (service as any).checkVehicleByName(vehicleName);
       expect(result).toBe(true);
-      expect((service as any).checkVehicleByName).toHaveBeenCalledWith(
-        vehicleName,
-      );
+      expect(repository.findByName).toHaveBeenCalledWith(vehicleName);
+    });
+
+    it('should return false if vehicle does not exist', async () => {
+      const vehicleName = 'Carro B';
+      jest.spyOn(repository, 'findByName').mockResolvedValueOnce(null);
+      const result = await (service as any).checkVehicleByName(vehicleName);
+      expect(result).toBe(false);
+      expect(repository.findByName).toHaveBeenCalledWith(vehicleName);
     });
   });
 });
