4e349f873ad2c32c07addc3ac1a929014d92133f
Joaquim Firmo <joaquimnt18@gmail.com>
2025-06-08 17:17:05 -0300

    feat: cria rota para criacao de transports.

--- src/transports/transports.controller.ts
+++ src/transports/transports.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body } from '@nestjs/common';
 import { TransportsService } from './transports.service';
 import { FiltersTransportDto } from './dto/filters-transport.dto';
+import { CreateTransportDto } from './dto/create-transport.dto';
 
 @Controller('transportes')
 export class TransportsController {
@@ -13,5 +14,10 @@ export class TransportsController {
   ) {
     const { page = 0, limit = 25, ...filters } = queryParams;
     return this.transportsService.findAll(filters, page, limit);
+  }
+
+  @Post()
+  create(@Body() transport: CreateTransportDto) {
+    return this.transportsService.create(transport);
   }
 }
